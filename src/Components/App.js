c1e191049f685f53a2b3725a638a324cd30c249a
Shahla Atiq <shahlatiq24@gmail.com>
2019-12-17 16:27:43 +0000

    first step

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Header from 'react';
+import TaskItem from 'react';
+import App from 'react';
 
 function App() {
   return (
