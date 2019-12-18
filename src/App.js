30021b0a27551dbdea51bbc11dd15313b40472a8
lindyloutama <ltamakaha@yahoo.com>
2019-12-18 00:59:09 +0000

    components folder removed, favicon added, errors to browser sorted

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+//import Header from './Header';
+//import TaskItem from './TaskItem';
+//import Restaurants from './Restaurants'
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     </header>
+
+<body>
+  <h1>FOODERS</h1>
+  <div class="container">
+  <p>We help you navigate the ever changing food scene. Your best friend now joined the V-gang? No problem! </p>
+<div class="row">
+
+
+
+</div>
+</div>
+
+</body>
+
+    </div>
+  );
+}
+
+export default App;
