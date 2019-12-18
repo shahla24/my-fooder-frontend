508008a5db3dc974d2c2917a36a25065bde97ce8
Shahla Atiq <shahlatiq24@gmail.com>
2019-12-18 20:14:05 +0000

    add description of fooder

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
 <div class="container">
   <h1 id="mainHeading">FOODERS</h1>
   <p>We help you navigate the ever changing food scene. Your best friend now joined the V-gang? No problem! </p>
+  <p>Your cousin has recently been diagnosed with coeliac? You only eat fish?</p>
+  <p>FOODERS are especially for you</p>
+
   <div class="row">
     <Header />
     <TaskItem />
