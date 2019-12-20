603d1db38cba67a117590ef6abd18633920cdbfb
Shahla Atiq <shahlatiq24@gmail.com>
2019-12-20 16:23:27 +0000

    remove gitignore files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import TaskItem from './TaskItem';
 import Restaurants from './Restaurants';
 import Booking from './Booking';
 import "./App.css";
-
+import Icon from 'react';
+import { render } from '@testing-library/react';
 
 
 function App() {
+
+  render()
+  {
+  
+  
   return (
 
 <div class="container">
@@ -15,17 +21,20 @@ function App() {
   <p>We help you navigate the ever changing food scene. Your best friend now joined the V-gang? No problem! </p>
   <p>Your cousin has recently been diagnosed with coeliac? You only eat fish?</p>
   <p>FOODERS are especially for you</p>
-
   <div class="row">
     <Header />
     <TaskItem />
     <Restaurants />
     <Booking />
-
+    <Icon />
 
     </div>
   </div>
+  
+  
+  
   );
+ } ;
 }
 
 export default App;
---
+++ src/Icon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Icon extends React.Component {
+
+    render() {
+
+        return (
+            <h1>
+                <header className="Fooder"></header>
+            </h1>
+        );
+    }
+ }
+    
+
+export default Icon;
