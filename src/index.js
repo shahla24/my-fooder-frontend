10407e109841ba55b4d7133037b12f98b6ed842b
lindyloutama <ltamakaha@yahoo.com>
2019-12-23 00:22:32 +0000

    package-lock json added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from '.Header';
+import Restaurants from 'Restaurants';
+import TaskItem from 'TaskItem';
 
-ReactDOM.render(<App />, 
+ReactDOM.render(
+<App />, 
+<Header />,
+<Restaurants />,
+<TaskItem />,
     document.getElementById('root')
     );
